b02a150ced02ba1142cca8d69a818eb8e1ce3ec0
andy <andersgee@gmail.com>
2022-10-18 09:57:51 +0200

    tidy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,20 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import { useState } from "react";
-import { InputFloat, InputInt, InputText } from "../components/Input";
 import { dateformat } from "../utils/date";
 import { AppRouterTypes, trpc } from "../utils/trpc";
 
 type Beef = NonNullable<AppRouterTypes["beef"]["read"]["output"]>;
 
+const inputStyle =
+  "w-[20ch] rounded-sm border border-slate-200 bg-transparent px-2 py-1 hover:border-blue-500 focus:outline-none focus:ring focus:ring-blue-500/40 active:ring active:ring-blue-500/40";
+
+/** Changing a key triggers a rerender (modify this to reset defaultValues) */
+let tableKey = 0;
+
 const Home: NextPage = () => {
   const { data: beefs, refetch: refetchBeefs } = trpc.beef.readAll.useQuery();
-  const { mutateAsync: createBeef } = trpc.beef.create.useMutation();
+  const { mutateAsync: createBeef, isLoading: createIsLoading } = trpc.beef.create.useMutation();
   const { mutateAsync: updateBeefs, isLoading: updateIsLoading } = trpc.beef.updateMultiple.useMutation();
   const { mutateAsync: deleteBeef, isLoading: deleteIsLoading } = trpc.beef.delete.useMutation();
 
@@ -17,19 +23,38 @@ const Home: NextPage = () => {
   const [myString, setMyString] = useState("");
   const [myOptionalString, setMyOptionalString] = useState("");
 
-  const [hasEditedIds, setHasEditedIds] = useState<string[]>([]);
-
   const [editedBeefs, setEditedBeefs] = useState<Beef[]>([]);
 
-  const handleSave = async () => {
+  const handleCreate = async () => {
+    await createBeef({ myFloat, myInt, myString, myOptionalString });
+    refetchBeefs();
+    setEditedBeefs([]);
+    tableKey += 1;
+  };
+
+  const handleDelete = async (id: string) => {
+    await deleteBeef({ id });
+    refetchBeefs();
+    setEditedBeefs((prev) => prev.filter((b) => b.id !== id));
+    tableKey += 1;
+  };
+
+  const handleSaveChanges = async () => {
     await updateBeefs(editedBeefs);
-    setHasEditedIds([]);
+    await refetchBeefs();
+    setEditedBeefs([]);
+    tableKey += 1;
+  };
+
+  const handleDiscardChanges = async () => {
+    await refetchBeefs();
+    setEditedBeefs([]);
+    tableKey += 1;
   };
 
   const handleEdit = (partialBeef: Partial<Beef>) => {
     if (!beefs || !partialBeef.id) return;
     const id = partialBeef.id;
-    setHasEditedIds((prev) => [...prev, id]);
 
     const dbBeef = beefs.find((beef) => beef.id === id);
     if (!dbBeef) return;
@@ -49,123 +74,179 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <div>
-        <h1>CRUD example</h1>
-        <button disabled={updateIsLoading} onClick={handleSave}>
-          save changes
-        </button>
-        <table className="">
-          <tbody>
-            <tr>
-              <th>createdAt</th>
-              <th>myFloat</th>
-              <th>myInt</th>
-              <th>myString</th>
-              <th>myOptionalString</th>
-            </tr>
-            <tr>
-              <td> </td>
-              <td>
-                <InputFloat value={myFloat} onChange={setMyFloat} />
-              </td>
-              <td>
-                <InputInt value={myInt} onChange={setMyInt} />
-              </td>
-              <td>
-                <InputText value={myString} onChange={setMyString} />
-              </td>
-              <td>
-                <InputText value={myOptionalString} onChange={setMyOptionalString} />
-              </td>
-              <td>
-                {/*disabled={!sessionData?.user} */}
-                <button
-                  onClick={async () => {
-                    await createBeef({ myFloat, myInt, myString, myOptionalString });
-                    refetchBeefs();
-                  }}
-                  className="bg-blue-600 px-2 py-1 font-semibold text-white disabled:bg-gray-500"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="h-6 w-6"
-                  >
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                  </svg>
-                </button>
-              </td>
-            </tr>
+      <Head>
+        <title>crudbeef</title>
+      </Head>
+      <div className="flex justify-center">
+        <div>
+          <h1 className="my-8 text-center text-4xl">crudbeef example</h1>
+          <button
+            disabled={editedBeefs.length === 0 || updateIsLoading}
+            onClick={handleSaveChanges}
+            className={`px-2 py-1 ${editedBeefs.length > 0 ? "bg-green-600" : ""} disabled:bg-neutral-400`}
+          >
+            save changes
+          </button>
 
-            {beefs?.map((beef) => {
-              const hasEdited = hasEditedIds.includes(beef.id);
-              return (
-                <tr
-                  key={beef.id}
-                  className={`${hasEdited ? "bg-orange-200 even:bg-orange-200" : "bg-neutral-100 even:bg-neutral-200"}`}
-                >
-                  <td>{dateformat(beef.createdAt)}</td>
-                  <td>
-                    <input
-                      type="number"
-                      className="bg-transparent"
-                      defaultValue={beef.myFloat}
-                      onChange={(e) => handleEdit({ id: beef.id, myFloat: parseFloat(e.target.value) })}
-                    />
-                  </td>
-                  <td>
-                    <input
-                      type="number"
-                      className="bg-transparent"
-                      defaultValue={beef.myInt}
-                      onChange={(e) => handleEdit({ id: beef.id, myInt: Math.floor(parseFloat(e.target.value)) })}
-                    />
-                  </td>
-                  <td>
-                    <input
-                      type="text"
-                      className="bg-transparent"
-                      defaultValue={beef.myString}
-                      onChange={(e) => handleEdit({ id: beef.id, myString: e.target.value })}
-                    />
-                  </td>
-                  <td>
-                    <input
-                      type="text"
-                      className="bg-transparent"
-                      defaultValue={beef.myOptionalString || ""}
-                      onChange={(e) => handleEdit({ id: beef.id, myOptionalString: e.target.value })}
-                    />
-                  </td>
-                  <td>
-                    <button
-                      disabled={deleteIsLoading}
-                      className="bg-red-600 px-2 py-1 font-semibold text-white disabled:bg-gray-500"
-                      onClick={async () => {
-                        await deleteBeef({ id: beef.id });
-                        refetchBeefs();
-                      }}
+          <button
+            disabled={editedBeefs.length === 0}
+            onClick={handleDiscardChanges}
+            className={`px-2 py-1 ${editedBeefs.length > 0 ? "bg-blue-600" : ""} disabled:bg-neutral-400`}
+          >
+            discard changes
+          </button>
+
+          <table className="" key={tableKey}>
+            <tbody>
+              <tr>
+                <th>createdAt</th>
+                <th>myFloat</th>
+                <th>myInt</th>
+                <th>myString</th>
+                <th>myOptionalString</th>
+              </tr>
+              <tr>
+                <td> </td>
+                <td>
+                  <input
+                    type="number"
+                    value={myFloat}
+                    onChange={(e) => {
+                      const number = parseFloat(e.target.value);
+                      if (isFinite(number)) setMyFloat(number);
+                    }}
+                    className={inputStyle}
+                  />
+                </td>
+                <td>
+                  <input
+                    type="number"
+                    value={myInt}
+                    onChange={(e) => {
+                      const number = parseFloat(e.target.value);
+                      if (isFinite(number)) setMyInt(Math.floor(number));
+                    }}
+                    className={inputStyle}
+                  />
+                </td>
+                <td>
+                  <input
+                    type="text"
+                    value={myString}
+                    onChange={(e) => {
+                      setMyString(e.target.value);
+                    }}
+                    className={inputStyle}
+                  />
+                </td>
+                <td>
+                  <input
+                    type="text"
+                    value={myOptionalString}
+                    onChange={(e) => {
+                      setMyOptionalString(e.target.value);
+                    }}
+                    className={inputStyle}
+                  />
+                </td>
+                <td>
+                  <button
+                    disabled={createIsLoading}
+                    onClick={handleCreate}
+                    className="bg-blue-600 px-2 py-1 font-semibold text-white disabled:bg-gray-500"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="h-6 w-6"
                     >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="h-6 w-6"
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                    </svg>
+                  </button>
+                </td>
+              </tr>
+
+              {beefs?.map((beef) => {
+                const hasEdited = editedBeefs.some((b) => b.id === beef.id);
+                return (
+                  <tr
+                    key={beef.id}
+                    className={`${
+                      hasEdited ? "bg-orange-200 even:bg-orange-200" : "bg-neutral-100 even:bg-neutral-200"
+                    }`}
+                  >
+                    <td>{dateformat(beef.createdAt)}</td>
+                    <td>
+                      <input
+                        type="number"
+                        className={inputStyle}
+                        defaultValue={beef.myFloat}
+                        onChange={(e) => {
+                          const number = parseFloat(e.target.value);
+                          if (isFinite(number)) {
+                            handleEdit({ id: beef.id, myFloat: number });
+                          }
+                        }}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="number"
+                        className={inputStyle}
+                        defaultValue={beef.myInt}
+                        onChange={(e) => {
+                          const number = Math.floor(parseFloat(e.target.value));
+
+                          if (isFinite(number)) {
+                            console.log("triggered handleEdit with myInt:", number);
+                            handleEdit({ id: beef.id, myInt: number });
+                          }
+                        }}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="text"
+                        className={inputStyle}
+                        defaultValue={beef.myString}
+                        onChange={(e) => handleEdit({ id: beef.id, myString: e.target.value })}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="text"
+                        className={inputStyle}
+                        defaultValue={beef.myOptionalString || undefined}
+                        onChange={(e) => handleEdit({ id: beef.id, myOptionalString: e.target.value })}
+                      />
+                    </td>
+                    <td>
+                      <button
+                        disabled={deleteIsLoading}
+                        className="bg-red-600 px-2 py-1 font-semibold text-white disabled:bg-gray-500"
+                        onClick={() => handleDelete(beef.id)}
                       >
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                      </svg>
-                    </button>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="h-6 w-6"
+                        >
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
